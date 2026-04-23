4e6caf36b661dff5d9cb53ef43df2346bdc33919
MuhammadFelda <ifelfelda27@gmail.com>
2026-04-23 13:29:27 +0700

    feat: Added http response handler

---
+++ backend/package/response/response.go
@@ -0,0 +1,59 @@
+package response
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type Envelope struct {
+	Status	string	`json:"status"`
+	Data	any		`json:"data"`
+	Error	*string	`json:"error"`
+}
+
+func Success(w http.ResponseWriter, status int, data any) {
+	write(w, status, Envelope{
+		Status: "success",
+		Data: data,
+		Error: nil,
+	})
+}
+
+func Created(w http.ResponseWriter, data any) {
+	Success(w, http.StatusCreated, data)
+}
+
+func Error(w http.ResponseWriter, status int, message string) {
+	write(w, status, Envelope{
+		Status: "error",
+		Data: nil,
+		Error: &message,
+	})
+}
+
+func BadRequest(w http.ResponseWriter, message string) {
+	Error(w, http.StatusBadRequest, message)
+}
+
+func Unauthorized(w http.ResponseWriter, message string) {
+	Error(w, http.StatusUnauthorized, message)
+}
+
+func Forbidden(w http.ResponseWriter, message string) {
+	Error(w, http.StatusForbidden, message)
+}
+
+func NotFound(w http.ResponseWriter, message string) {
+	Error(w, http.StatusNotFound, message)
+}
+
+func InternalError(w http.ResponseWriter) {
+	msg := "An internal error occured!"
+	Error(w, http.StatusInternalServerError, msg)
+}
+
+func write(w http.ResponseWriter, status int, env Envelope) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	_ = json.NewEncoder(w).Encode(env)
+}
