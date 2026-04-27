ec28cca2bed983d3ce970b16fef0e251b592ac0f
MuhammadFelda <ifelfelda27@gmail.com>
2026-04-27 16:15:29 +0700

    fix: Change id data type

--- backend/internal/domains/material.go
+++ backend/internal/domains/material.go
@@ -3,7 +3,7 @@ package domains
 import "time"
 
 type Material struct {
-	ID				string		`json:"id"`
+	ID				int			`json:"id"`
 	UserID			string		`json:"user_id"`
 	Name			string		`json:"name"`
 	Description		*string		`json:"description"`
