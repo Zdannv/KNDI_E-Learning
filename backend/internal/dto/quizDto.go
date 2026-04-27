4096ce050e525892d92e218a970bb6dbf7c1d24c
MuhammadFelda <ifelfelda27@gmail.com>
2026-04-27 16:18:12 +0700

    fix: Minor change on description data type

--- backend/internal/dto/quizDto.go
+++ backend/internal/dto/quizDto.go
@@ -2,11 +2,11 @@ package dto
 
 type CreateQuizRequest struct {
 	Title			string		`json:"title"`
-	Description		string		`json:"description"`
+	Description		*string		`json:"description"`
 }
 
 type UpdateQuizRequest struct {
 	Title			string		`json:"title"`
-	Description		string		`json:"description"`
+	Description		*string		`json:"description"`
 	IsPublished		bool		`json:"is_published"`
 }
