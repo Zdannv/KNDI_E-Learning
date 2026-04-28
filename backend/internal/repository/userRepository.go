9c2c85ff98709d1aec8f5fccf27eb2a70527986a
MuhammadFelda <ifelfelda27@gmail.com>
2026-04-28 16:44:05 +0700

    fix: Minor change

--- backend/internal/repository/userRepository.go
+++ backend/internal/repository/userRepository.go
@@ -11,10 +11,26 @@ import (
 )
 
 const (
-	insertUser				= `INSERT INTO users (username, email, password) VALUES ($1, $2, $3) RETURNING id, created_at, updated_at`
-	selectUserByID			= `SELECT id, username, password, role, created_at, updated_at FROM users WHERE id = $1`
-	selectUserByUsername	= `SELECT id, username, password, role, created_at, updated_at FROM users WHERE username = $1`
-	usernameExist			= `SELECT EXIST(SELECT 1 FROM users WHERE username = $1)`
+	insertUser           = `
+		INSERT INTO users (username, email, password, role) 
+		VALUES ($1, $2, $3, $4) 
+		RETURNING id, created_at, updated_at`
+	selectUserByID       = `
+		SELECT id, username, email, password, role, created_at, updated_at 
+		FROM users 
+		WHERE id = $1`
+	selectUserByUsername = `
+		SELECT id, username, email, password, role, created_at, updated_at 
+		FROM users 
+		WHERE username = $1`
+	usernameExist        = `
+		SELECT COUNT(*) > 0 
+		FROM users 
+		WHERE username = $1`
+	emailExist           = `
+		SELECT COUNT(*) > 0 
+		FROM users 
+		WHERE email = $1`
 )
 
 type UserRepository interface {
@@ -22,6 +38,7 @@ type UserRepository interface {
 	FindByID(ctx context.Context, id string) (*domains.User, error)
 	FindByUsername(ctx context.Context, username string) (*domains.User, error)
 	UsernameExists(ctx context.Context, username string) (bool, error)
+	EmailExists(ctx context.Context, email string) (bool, error)
 }
 
 type userRepository struct {
@@ -33,8 +50,8 @@ func NewUserRepository(pool *pgxpool.Pool) UserRepository {
 }
 
 func (r *userRepository) Create(ctx context.Context, u *domains.User) error {
-	err := r.pool.QueryRow(ctx, insertUser, u.Username, u.Password, u.Role).
-				Scan(&u.ID, &u.CreatedAt, &u.UpdatedAt)
+	err := r.pool.QueryRow(ctx, insertUser, u.Username, u.Email, u.Password, u.Role).
+		Scan(&u.ID, &u.CreatedAt, &u.UpdatedAt)
 	if err != nil {
 		return fmt.Errorf("UserRepo.Created: %w", err)
 	}
@@ -45,7 +62,7 @@ func (r *userRepository) Create(ctx context.Context, u *domains.User) error {
 func (r *userRepository) FindByID(ctx context.Context, id string) (*domains.User, error) {
 	u := &domains.User{}
 	err := r.pool.QueryRow(ctx, selectUserByID, id).
-			Scan(&u.ID, &u.Username, &u.Password, &u.Role, &u.CreatedAt, &u.UpdatedAt)
+		Scan(&u.ID, &u.Username, &u.Password, &u.Role, &u.CreatedAt, &u.UpdatedAt)
 	if err != nil {
 		if errors.Is(err, pgx.ErrNoRows) {
 			return nil, ErrorNotFound
@@ -59,7 +76,7 @@ func (r *userRepository) FindByID(ctx context.Context, id string) (*domains.User
 func (r *userRepository) FindByUsername(ctx context.Context, username string) (*domains.User, error) {
 	u := &domains.User{}
 	err := r.pool.QueryRow(ctx, selectUserByUsername, username).
-			Scan(&u.ID, &u.Username, &u.Password, &u.Role, &u.CreatedAt, &u.UpdatedAt)
+		Scan(&u.ID, &u.Username, &u.Password, &u.Role, &u.CreatedAt, &u.UpdatedAt)
 	if err != nil {
 		if errors.Is(err, pgx.ErrNoRows) {
 			return nil, ErrorNotFound
@@ -77,5 +94,14 @@ func (r *userRepository) UsernameExists(ctx context.Context, username string) (b
 		return false, fmt.Errorf("UserRepo.UsernameExists: %w", err)
 	}
 
+	return exists, nil
+}
+
+func (r *userRepository) EmailExists(ctx context.Context, email string) (bool, error) {
+	var exists bool
+	err := r.pool.QueryRow(ctx, emailExist, email).Scan(&exists)
+	if err != nil {
+		return false, fmt.Errorf("userRepo.EmailExists: %w", err)
+	}
 	return exists, nil
 }
