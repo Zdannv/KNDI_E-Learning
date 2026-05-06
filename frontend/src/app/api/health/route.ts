590c0b99736fd262a007c66d9a1109c0f374f11b
MuhammadFelda <ifelfelda27@gmail.com>
2026-05-06 16:33:33 +0700

    fix: major change

--- frontend/src/app/api/health/route.ts
+++ frontend/src/app/api/health/route.ts
@@ -1,14 +1,27 @@
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  try {
-    const res = await fetch(`${process.env.INTERNAL_API_URL}/health`);
-    const data = await res.json();
-    return NextResponse.json(data, { status: res.status });
-  } catch (error) {
+  const backendUrl = process.env.INTERNAL_API_URL
+
+  if (!backendUrl) {
     return NextResponse.json(
-      { status: "error", error: "Backend unreachable" },
+      { "status": "error", "error": "Environtment variable is not set" },
       { status: 503 }
-    );
+    )
+  }
+  
+  try {
+    const res = await fetch(`${backendUrl.replace(/\/$/, "")}/health`, {
+      signal: AbortSignal.timeout(5000)
+    })
+
+    const data = await res.json()
+
+    return NextResponse.json(data, { status: res.status })
+  } catch (err) {
+    return NextResponse.json(
+      { "status": "error", error: "Backend unreachable" },
+      { status: 503}
+    )
   }
 }
